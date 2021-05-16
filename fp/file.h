2801c7b3cb1bbfd5ed4c8ce52136f70d20699c0c
tqfx <tqfx@foxmail.com>
2021-05-17 01:53:05 +0800

    Add comment on file.h

--- fp/file.h
+++ fp/file.h
@@ -47,12 +47,45 @@
 
 __BEGIN_DECLS
 
+/**
+ * @brief          Read the entire contents of the binary file
+ * @param[in]      filename: File name to be operated
+ * @param[out]     dst: The address where the first address of data is stored
+ * @param[out]     sz: The address where the number of data is stored
+ * @return         int 0(success) -1(failure)
+*/
 extern int file_br(const char *filename, void **dst, size_t *sz);
+
+/**
+ * @brief          Write data to binary file
+ * @param[in]      filename: File name to be operated
+ * @param[out]     dst: The first address of data to be written
+ * @param[out]     sz: The number of data to be written
+ * @return         int 0(success) -1(failure)
+*/
 extern int file_bw(const char *filename, void *src, size_t sz);
 
+/**
+ * @brief          Read the entire contents of the text file
+ * @param[in]      filename: File name to be operated
+ * @param[out]     dst: The address where the first address of string
+ * @return         int 0(success) -1(failure)
+*/
 extern int file_tr(const char *filename, char **dst);
+
+/**
+ * @brief          Write data to text file
+ * @param[in]      filename: File name to be operated
+ * @param[out]     dst: The first address of string
+ * @return         int 0(success) -1(failure)
+*/
 extern int file_tw(const char *filename, char *src);
 
+/**
+ * @brief          Check if the file exists
+ * @param[in]      filename: File name to be operated
+ * @return         int 1(yes) 0(no)
+*/
 extern int file_exist(const char *filename);
 
 __END_DECLS
